d90fdc468f7ea1d3cb23d413373d6f75a21f6049
Anubhab2003 <anubhabchowdhurychowdhury01@gmail.com>
2024-07-28 20:46:05 +0530

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-pink-600'>
+      I am Dr Doom
+    </div>
+  );
+}
+
+export default App;
